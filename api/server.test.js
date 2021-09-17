f6be43831dbc6030a6cfcf7afa1780bd2aaf2adc
ashleyannlaz <paperpastels@gmail.com>
2021-09-17 03:14:50 -0400

    2 Tests Finished

--- api/server.test.js
+++ api/server.test.js
@@ -1,39 +1,67 @@
-test('sanity', () => {
-  expect(true).toBe(true)
-})
+const request = require("supertest");
+const server = require("../api/server");
+const db = require("../data/dbConfig");
+const jwtDecode = require("jwt-decode");
 
+beforeAll(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+});
 
+test("sanity", () => {
+  expect(true).toBe(true);
+});
 
-describe('Register', ()=> {
-  test('Returns id username, and password object', () => {
+describe("Register", () => {
+  test("Returns invalid credentials if incorrect username or password", async () => {
+    let res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "Leito", password: "1234" });
+    expect(res.body.message).toMatch(/invalid credentials/i);
+    expect(res.status).toBe(401);
+    res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "Emma", password: "12345" });
+    expect(res.body.message).toMatch(/invalid credentials/i);
+    expect(res.status).toBe(401);
+  }, 750);
 
-  })
-  
-  test('If missing username or password returns (username and password required)', () => {
+  test("If missing username or password returns (username and password required)", async () => {
+    let res = await request(server)
+      .post("/api/auth/login")
+      .send({ password: "1234" });
+    expect(res.body.message).toMatch(/username and password required/i);
+    expect(res.status).toBe(401);
+    res = await request(server)
+      .post("/api/auth/login")
+      .send({ username: "Leonardo" });
+    expect(res.body.message).toMatch(/username and password required/i);
+    expect(res.status).toBe(401);
+  }, 750);
+});
+
+describe("Login", () => {
+  test("If login failed due to username or password, invalid creds", () => {
     
-  })
-  
-  test('username taken', () => {
-    
-  })
-})
+  });
+  test("If login failed due to username or password, invalid creds", () => {});
 
-describe('Login', () => {
-  test('Response has message and token', () => {
-    
-  })
-  test('If login failed due to username or password, invalid creds', () => {
-    
-  })
-})
+});
 
-describe('Jokes', () => {
-  test('Response has message and token', () => {
-    
-  })
-  test('If login failed due to username or password, invalid creds', () => {
-    
-  })
-})
 
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkphbmUiLCJpYXQiOjE2MzE4NTY3MTksImV4cCI6MTYzMTk0MzExOX0.Zhg0jpQv4QLY-hszIQu40sjE1L6RWOBhEaRkW0_9KVw
+describe("Jokes", () => {
+  test("Responds with dad jokes if given valid token", async () => {
+
+    let res = await request(server).post('/api/auth/login').send({ username: 'Emi', password: '1234' })
+    res = await request(server).get('/api/jokes').set('Authorization', res.body.token)
+    expect(res.body).toMatch(/tired of following my dreams/i)
+  }, 750)
+
+  test("Returns [Token required] if none exists", async () => {
+    const res = await request(server).get("/api/jokes");
+
+    expect(res.body).toMatchObject({
+      message: "Token required",
+    });
+  });
+});
